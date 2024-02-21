1cc01fcc7fc89248b792173b12a33c2f9e4955d8
Alexey <swiftslayeer@gmail.com>
2024-02-22 02:39:43 +0300

    Выполнил проект до создания репозитория, ну ниче ща сделаю рефакторинг кода и залью в ветку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,19 +1,62 @@
 //
 //  ViewController.swift
-//  Counter
+//  newApp
 //
-//  Created by Алексей Шишкарев on 22.02.2024.
+//  Created by Алексей Шишкарев on 16.02.2024.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ChangeLog: UITextView!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var changeButtonBlue: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeButton.backgroundColor = UIColor.red
+        countLabel.text = "Значение счетчика: 0"
+        ChangeLog.text = "История изменений:"
     }
-
-
+    
+    func format() -> String {
+        let currentDate = Date()
+        let DateFormato = DateFormatter()
+        DateFormato.dateFormat = "HH:mm:ss"
+        let formattedTime = DateFormato.string(from: currentDate)
+        return formattedTime
+    }
+    
+  
+    var number = 0
+  
+    @IBAction func buttonTouch(_ sender: Any) {
+        number += 1
+        countLabel.text = "Значение счетчика: \(number)"
+        ChangeLog.text += "\n[\(format())]: Значение изменено на +1"
+    }
+    
+    @IBAction func buttonTouchBlue(_ sender: Any) {
+        
+        if number > 0 {
+            number -= 1
+            ChangeLog.text += "\n[\(format())]: Значение изменено на -1"
+        } else {
+            number = 0
+            ChangeLog.text += "\n[\(format())]: Попытка уменьшить значение счётчика ниже 0"
+        }
+        countLabel.text = "Значение счетчика: \(number)"
+    }
+    
+    @IBAction func resetBtn(_ sender: Any) {
+        
+        number = 0
+        countLabel.text = "Значение счетчика: \(number)"
+        ChangeLog.text += "\n[\(format())]: Значение сброшено"
+    }
+    
 }
+
 
